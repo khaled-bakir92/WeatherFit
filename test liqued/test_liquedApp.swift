99241ca70c02de799f6950ad28bb8644ded1ecb8
khaled-bakir92 <khaled.bakeer44@gmail.com>
2026-01-14 22:45:59 +0300

    Initial Commit

---
+++ test liqued/test_liquedApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_liquedApp.swift
+//  test liqued
+//
+//  Created by khaled Bakir on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct test_liquedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test liquedTests/test_liquedTests.swift
@@ -0,0 +1,17 @@
+//
+//  test_liquedTests.swift
+//  test liquedTests
+//
+//  Created by khaled Bakir on 1/14/26.
+//
+
+import Testing
+@testable import test_liqued
+
+struct test_liquedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
